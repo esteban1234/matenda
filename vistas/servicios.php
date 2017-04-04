7232076573ce4cb6081349837f9750533a518bbc
esteban1234 <tva_libra@hotmail.com>
2017-04-04 16:42:09 -0500

    servicios listo

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -1,6 +1,75 @@
 <!DOCTYPE html>
 <?php include 'encabezado2.php' ?>
 <body>
+
+<div class="navbar navbar-default navbar-static-top contenmenu">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="">
+              <a href="../index.php"><i class="fa fa-home fa-fw"></i>&nbsp;INDEX</a>
+            </li>
+            <li>
+              <a href="nosotros.php"><i class="fa fa-users fa-fw"></i>&nbsp;NOSOTROS</a>
+            </li>
+            <li class="active">
+              <a href="servicios.php"><i class="fa fa-cogs fa-fw"></i>&nbsp;SERVICIOS</a>
+            </li>
+            <li>
+              <a href="contacto.php"><i class="fa fa-fax fa-fw"></i>&nbsp;CONTACTO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="bannerser">
+    	<p>SERVICIOS</p>
+    </div>
+    <article class="bannerdirec">
+ 	<p>Somos una empresa inmobiliaria dedicada a prestar servicios de asesoría en bienes raíces.</p>
+ </article>
+
+ <h2 class="titulo"><p>¿Quieres vender una propiedad?</p></h2>
+ <article class="bannerdirec">
+ 	<p>Los servicos que ofrecemos son los siguientes:</p>
+ </article>
+
+ <div class="contenpuntos">
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Asesoria en documentos para que la negociación sea rápida.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Los impuestos que debe pagar o la forma de excentarlos.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Asesoria en la forma en la que operan los créditos y en qué tiempo le pagará la institución crediticia en caso de que acepte.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Compromiso de venta en un tiempo establecido.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Asesoria para determinar el precio de venta de su propiedad, sustentada en precios reales y actuales.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Atención personal a cada cliente interesado en adquirir su propiedad; igualmente se le hará llegar una carta de intención de compra con la oferta que haga el cliente.</p>
+ </div>
+
+
+
+
+ <h2 class="titulo"><p>¿Quieres rentar una propiedad?</p></h2>
+ <article class="bannerdirec">
+ 	<p>Los servicos que ofrecemos son los siguientes:</p>
+ </article>
+
+ <div class="contenpuntos">
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Siempre es bienvenida, escuchada y considerada una oferta, siempre y cuando haya visitado previamente la propiedad, cuente con el crédito autorizado o la suma total del importe que oferte, así como que esté seriamente interesado en adquirir la vivienda.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Con un presupuesto definido, programaremos una cita para que visite las opciones acordes a sus necesidades y presupuesto.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Conoce las diversas opciones disponibles de acuerdo a su presupuesto, necesidades e interés en poco tiempo.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>La documentación de las propiedades que ofrecemos está en regla, lista para formalizar la compra-venta inmediatamente.</p>
+ 	<!-- <p><i class="fa fa-check" aria-hidden="true"></i>Asesoria para determinar el precio de venta de su propiedad, sustentada en precios reales y actuales.</p>
+ 	<p><i class="fa fa-check" aria-hidden="true"></i>Atención personal a cada cliente interesado en adquirir su propiedad; igualmente se le hará llegar una carta de intención de compra con la oferta que haga el cliente.</p> -->
+ </div>
+
+ <p class="boton2"><a href="contacto.php">Contáctanos</a></p>
 	
 <a class="go-top" href="#">Ir arriba</a>
 <?php include 'pie.php' ?>
