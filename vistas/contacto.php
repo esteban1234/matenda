1d668e7030c5d51ecbf7cd3cb9a1392e7e333512
esteban1234 <tva_libra@hotmail.com>
2017-04-06 09:30:35 -0500

    fin de pagina

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -41,6 +41,45 @@
     <article class="bannerdirec">
   <p>Oficina MATENDA PROMOTORA S. de R.L de C.V. E-Mail: <a href="mailto:info@glg.com.mx">info@pendiente</a> Teléfono: pendiente. Calle 3 #235 por 32 y 36 Col. Campestre, Mérida (Yucatán). C.P.97120</p>
  </article>
+
+    <section id="contact">
+      <div class="section-content">
+        <h1 class="section-header">Contacta con <span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s">MATENDA PROMOTORA</span></h1>
+        <h3>Para más información acerca de nuestros servicios, por favor contáctenos usando este formulario.</h3>
+      </div>
+      <div class="contact-section">
+      <div class="container">
+        <form>
+          <div class="col-md-6 form-line">
+              <div class="form-group">
+                <label for="exampleInputUsername">Nombre (s)</label>
+                <input type="text" class="form-control" id="" placeholder="Escribe tu nombre completo">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail">E-mail</label>
+                <input type="email" class="form-control" id="exampleInputEmail" placeholder="Escribe tu correo electronico">
+              </div>  
+              <div class="form-group">
+                <label for="telephone">Teléfono</label>
+                <input type="tel" class="form-control" id="telephone" placeholder="Escribe tu numero teléfonico">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for ="description">Comentario</label>
+                <textarea  class="form-control" id="description" placeholder="Escribenos tu mensaje"></textarea>
+              </div>
+              <div>
+
+                <button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp;Enviar Mensaje</button>
+              </div>
+              
+          </div>
+        </form>
+      </div>
+    </section>
+
+    
 	
 <a class="go-top" href="#">Ir arriba</a>
 <?php include 'pie.php' ?>
--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -6,10 +6,10 @@
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>MATENDA PROMOTORA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
-  	<META NAME="Title" content="AMBIENSUP ANALISTAS PROFESIONALES en ciudad de mérida.">
+  	<META NAME="Title" content="MATENDA PROMOTORA en ciudad de mérida.">
   	<META NAME="Author" CONTENT="[]"/>
-  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, yucatan, dedicados a ofrecer servicios de consultoría en medio ambiente, Servicios de levantamiento geofísico, Servicios de arquitectura."/>
-  	<META NAME="Keywords" CONTENT="consultoría en medio ambiente en mérida, servicios de levantamiento geofísico en mérida, consultoría en medio ambiente, consultoría en medio ambiente en mérida yucatan, servicios de levantamiento geofísico en merida yucatan, servicios de levantamiento geofísico, servicios de arquitectura."/>
+  	<META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, yucatan, dedicados a ofrecer servicios de venta de inmobiliarios, Servicios de renta de inmobiliarios."/>
+  	<META NAME="Keywords" CONTENT="venta de inmobiliarios en mérida, renta de inmobiliarios en mérida, renta de inmobiliarios, renta de inmobiliarios en mérida yucatan, venta de inmobiliarios en merida yucatan."/>
   	<link rel="stylesheet" href="../css/bootstrap.css">
   	<link rel="stylesheet" href="../css/iconos.min.css">
 	<link rel="stylesheet" href="../css/estilos.css">
