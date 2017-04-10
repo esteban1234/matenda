a5447ecefbcb377bf517658067d72372e339e09d
champiiy <albert1047@hotmail.com>
2017-04-10 09:59:48 -0500

    UPDATE CONTACTO

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -2,7 +2,7 @@
 <?php include 'encabezado2.php' ?>
 <body>
 
-<div class="navbar navbar-default navbar-static-top contenmenu">
+    <div class="navbar navbar-default navbar-static-top contenmenu">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
@@ -33,49 +33,48 @@
 
     <div class="bannercont">
       <p>Contacto</p>
-      <!-- <div class="col-md-6">
-        <img src="../img/matenda.png" alt="MATENDA PROMOTORA" class="img-responsive">
-      </div> -->
     </div>
 
     <article class="bannerdirec">
-  <p>Oficina MATENDA PROMOTORA S. de R.L de C.V. E-Mail: <a href="mailto:info@glg.com.mx">info@pendiente</a> Teléfono: pendiente. Calle 3 #235 por 32 y 36 Col. Campestre, Mérida (Yucatán). C.P.97120</p>
- </article>
+      <p>Oficina MATENDA PROMOTORA S. de R.L de C.V. E-Mail: <a href="mailto:info@glg.com.mx">info@pendiente</a> Teléfono: pendiente. Calle 3 #235 por 32 y 36 Col. Campestre, Mérida (Yucatán). C.P.97120</p>
+    </article>
 
     <section id="contact">
       <div class="section-content">
-        <h1 class="section-header">Contacta con <span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s">MATENDA PROMOTORA</span></h1>
+        <h1 class="section-header">Contacta con <span class="content-header wow fadeIn" data-wow-delay="0.2s" data-wow-duration="2s">MATENDA PROMOTORA</span></h1>
         <h3>Para más información acerca de nuestros servicios, por favor contáctenos usando este formulario.</h3>
       </div>
+
       <div class="contact-section">
-      <div class="container">
-        <form action="envia.php" method="post">
-          <div class="col-md-6 form-line">
-              <div class="form-group">
-                <label for="exampleInputUsername">Nombre (s)</label>
-                <input type="text" class="form-control" id="" name="nombre" placeholder="Escribe tu nombre completo" required/>
+        <div class="container">
+          <form action="envia.php" method="post">
+              <div class="col-xs-12 col-sm-6 col-md-6 form-line">
+                <div class="form-group">
+                  <label for="exampleInputUsername">Nombre (s)</label>
+                  <input type="text" class="form-control" id="" name="nombre" placeholder="Escribe tu nombre completo" required/>
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputEmail">E-mail</label>
+                  <input type="email" class="form-control" id="exampleInputEmail" name="correo" placeholder="Escribe tu correo electronico" required/>
+                </div> 
+
+                <div class="form-group">
+                  <label for="telephone">Teléfono</label>
+                  <input type="tel" class="form-control" id="telephone" name="telefono" placeholder="Escribe tu numero teléfonico" required/>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="exampleInputEmail">E-mail</label>
-                <input type="email" class="form-control" id="exampleInputEmail" name="correo" placeholder="Escribe tu correo electronico" required/>
-              </div>  
-              <div class="form-group">
-                <label for="telephone">Teléfono</label>
-                <input type="tel" class="form-control" id="telephone" name="telefono" placeholder="Escribe tu numero teléfonico" required/>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for ="description">Comentario</label>
-                <textarea  class="form-control" id="description" name="comentario" placeholder="Escribenos tu mensaje" required/></textarea>
-              </div>
-              <div>
+
+              <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="form-group">
+                  <label for="description">Comentario</label>
+                  <textarea class="form-control" id="description" name="comentario" placeholder="Escribenos tu mensaje" required></textarea>
+                </div>
 
                 <button type="submit" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp;Enviar Mensaje</button>
-              </div>
-              
-          </div>
-        </form>
+              </div>              
+          </form>
+        </div>
       </div>
     </section>
 
